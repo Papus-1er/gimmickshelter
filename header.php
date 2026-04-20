4300b2831b43494e800925fd7d60c6379c1516d4
Papus-1er <adrien.masseron@hotmail.com>
2026-04-20 23:11:52 +0200

    fix header

--- header.php
+++ header.php
@@ -25,8 +25,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <?php wp_head(); ?>
-
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/favicon/favicon-16x16.png">
